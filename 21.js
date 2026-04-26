57c266f9a4aaf9c1b1fd0ae702ebec588f9dbdeb
nagi46-2 <ivanpodkorytov9959@gmail.com>
2026-04-26 22:24:05 +0500

    Create 21.js

---
+++ 21.js
@@ -0,0 +1,34 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/nagi46-2/NagiRepa/refs/heads/7/1_PoaAF.png",
+
+  "https://raw.githubusercontent.com/nagi46-2/NagiRepa/refs/heads/7/2_AS.png",
+
+  "https://raw.githubusercontent.com/nagi46-2/NagiRepa/refs/heads/7/3_MA.png",
+
+  "https://raw.githubusercontent.com/nagi46-2/NagiRepa/refs/heads/7/4_HW.png",
+
+  "https://raw.githubusercontent.com/nagi46-2/NagiRepa/refs/heads/7/5_TGAoG.png",
+
+  "https://raw.githubusercontent.com/nagi46-2/NagiRepa/refs/heads/7/6_EATME.png"
+
+];
+
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+
+const FACE_NAMES = [
+
+  "PORTRAIT",
+
+  "ANTICHRIST",
+
+  "MECHANICAL",
+
+  "HOLY",
+
+  "GOLDENAGE",
+
+  "EATME"
+
+];
+
